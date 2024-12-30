a97747f1f578df8e4772cb69707ba2180abe4a4c
Sebastian Badinas <srbadinas@gmail.com>
2024-12-31 06:00:22 +0800

    Added logger

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 const app = require('./app');
 const config = require('./config/config');
+const logger = require('./config/logger');
 
 let server;
 
-server = app.listen(config.port);
+server = app.listen(config.port, () => {
+    logger.info(`Listening to port ${config.port} ${config.env}`);
+});
